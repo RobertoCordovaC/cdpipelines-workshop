d6038eeaf116d877649d9a022b1cb169825fbb5d
faustocv <fausto.castaneda.v@gmail.com>
2017-08-06 15:55:36 -0500

    Fixes bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ pip install jinja2-cli
 
 echo "Exploding templates"
 jinja2 package.json.tmpl -D slidename=$1 > package.json
-jinja2 public/content.md.tmpl -D title="$2" > public/content.md
+jinja2 public/content/content.md.tmpl -D title="$2" > public/content/content.md
 jinja2 public/index.html.tmpl -D title="$2" > public/index.html
 
 echo "Installing dependencies"
