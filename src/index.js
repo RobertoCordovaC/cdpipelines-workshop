9b4020e68159a63ed6df355730bcd275a0e7bb31
faustocv <fausto.castaneda.v@gmail.com>
2017-08-06 15:43:37 -0500

    Refactors content and assets

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ script('https://remarkjs.com/downloads/remark-latest.min.js', 'loader')
 
 script.ready('loader', () => {
   var slideshow = remark.create({
-    sourceUrl: 'content.md'
+    sourceUrl: './content/content.md'
   })
 });
